07addedc6cf2179c4e0635b92823f5fba9df692e
Manoel E. S. S. <avlis@Avlis.Avlis>
2026-04-09 23:02:58 -0300

    datas - add data types python folder

---
+++ datas_python/dataTypes.py
@@ -0,0 +1,36 @@
+nome = "Alice"
+idade = 30
+cidade = "São Paulo"
+altura = 1.65 
+
+
+print("Nome :", nome)
+print("Idade :", idade)
+print("Cidade :", cidade)
+print("Altura :", altura)
+
+#  Este código define quatro variáveis: nome, idade, cidade e altura, e depois imprime seus valores usando a função print().
+#  As variáveis armazenam informações sobre uma pessoa, como seu nome, idade, cidade de residência e altura. O código é simples e direto,
+#  como criar e usar variáveis em Python, definir e imprimir informações. 
+#  O código é útil para demonstrar como armazenar e exibir informações em Python, e pode ser facilmente modificado 
+#  para incluir mais variáveis ou diferentes tipos de dados.
+#  O código é um exemplo básico de como usar variáveis em Python para armazenar e exibir informações.
+#  Ele pode ser expandido para incluir mais detalhes ou diferentes tipos de dados, dependendo das necessidades do usuário.
+
+
+nome1 = "Alice"
+idade1 = 30
+cidade1 = "São Paulo"
+altura1 = 1.65 
+
+
+print(type(nome1))
+print(type(idade1))
+print(type(cidade1))
+print(type(altura1))
+
+#  Este código é semelhante ao anterior, mas agora ele usa a função '''type()''' para mostrar o tipo de cada variável.
+#  A função '''type()''' retorna o tipo de dado de cada variável, como str para strings e int para inteiros. 
+#  O código é útil para entender os tipos de dados em Python e como eles são usados para armazenar informações. 
+#  Ele pode ser expandido para
+#  incluir mais variáveis ou diferentes tipos de dados, dependendo das necessidades do usuário. 
---
+++ datas_python/readme.md
@@ -0,0 +1,15 @@
+## Estudo aplicado desde os fundamentos básicos de Python para a universidade 
+
+# Conceitos de algortimos, resolução de problemas e entendimento de uso da linguagem.
+
+---
+
+Este conteudo aborda:
+
+- > Dados
+- > Manipulação de dados 
+- > Tipos de dados 
+- > Funções Básicas de input e output 
+
+
+- > Programa funcional minimo usando todas as informações aprendidas durante a sessão usando o terminal.
